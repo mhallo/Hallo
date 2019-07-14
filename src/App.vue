f3e6d0ba1214cd30d0df5bb03c51676c729f9d81
Matt Hallowell <matthewhallowell1@gmail.com>
2019-07-14 17:13:00 -0500

    walk before you can run...

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,9 @@
       <router-link to="/about">About</router-link> |
       <router-link to="/whoami">Who am I?</router-link>
     </div>
-    <fade-transition :duration="200">
-      <router-view/>
-    </fade-transition>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import {FadeTransition} from 'vue2-transitions'
-
-export default {
-  components:
-  {
-    FadeTransition
-  },
-  data() {
-    return {
-      show:true
-    }
-  }
-}
-</script>
 
 <style>
 #app {
@@ -48,28 +30,4 @@ export default {
 #nav a.router-link-exact-active {
   color: #42b983;
 }
-/* 
-body 
-{
-  background: rgb(233,233,233);
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-}
-
-html, body
-{
-  height: calc(100% - 50px);
-}
-
-#app
-{
-  background: #fff;
-  width: 50%;
-  padding: 30px;
-  border-radius: 10px;
-  margin: 50px auto 0 auto;
-  height: calc(60%-50px);
-} */
-.fade-move{
-    transition: transform .3s ease-out;
-  }
 </style>
