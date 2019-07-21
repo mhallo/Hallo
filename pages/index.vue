3c430b5983571ea0b1e094461eb06356c7f6f336
Matt Hallowell <matthewhallowell1@gmail.com>
2019-07-20 23:56:52 -0500

    removed boilerplate junk

--- pages/index.vue
+++ pages/index.vue
@@ -3,27 +3,11 @@
     <div>
       <logo />
       <h1 class="title">
-        hallo
+        Hallo
       </h1>
       <h2 class="subtitle">
-        hallo
+        Bjark
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
